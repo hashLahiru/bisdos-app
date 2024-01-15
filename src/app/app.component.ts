f2be58e262e4388374a51291e110ff76fdc52fdd
sameera <sameeramadhushan62220985@gmail.com>
2024-01-15 16:43:28 +0530

    tailwind installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+// angular core
+import {Component} from '@angular/core';
+// angular common
+import {CommonModule} from '@angular/common';
+// angular router
+import {RouterOutlet} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bisdos-apk';
 }
